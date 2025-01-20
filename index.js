8f82ca66370734f8ccce12b683fb3af66f10cf09
Kevin Baliat <kevinbaliat@Kevs-MacBook-Air.local>
2025-01-19 22:57:10 -0800

    counterEl fixes

---
+++ index.js
@@ -0,0 +1,33 @@
+// document.getElementById("count-el").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
+
+// camelCase
+let countEl = document.getElementById("count-el")
+
+let totalSaved = document.getElementById("total-saved")
+
+let saveMsg = "Previous entries: "
+
+let lastSave = null
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    // console.log(count)
+    // lastSave = saveMsg+totalSaved.innerText
+    // totalSaved.innerText += count+" - "
+    totalSaved.textContent += count+" - "
+
+    //sdt variable name and text name to 0
+    countEl.textContent=0
+    count = 0
+}
+
+//Google
+// innerText alternative mdn ... to find alternative components
